bdf176a384c8f736eb92f8976ec07b08ff87ce66
RAGHAVENDRA K H <23m0013@iitb.ac.in>
2024-08-16 20:21:43 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,26 +1,15 @@
-"""Expo URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, include
-
-
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-     path('',include('main.urls')),
-     path('',include('django.contrib.auth.urls')) , 
-     
-]
+    path('home',views.index, name='index'),
+    path('',views.index, name='index'),
+    path('sign_up',views.sign_up, name='sign_up'),
+    path('register',views.Register, name='Register'),
+    path('userinfo',views.Userinfo, name='Userinfo'),
+    path('news',views.News, name='news'),
+    path('About',views.About,name='about')
+
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
